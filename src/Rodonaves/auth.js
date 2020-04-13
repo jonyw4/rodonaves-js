4ce0f0b57ec0281a6d66f02296cf6e7f0e0ecfa7
Jonathan Célio <jonathan.clio@hotmail.com>
2020-04-13 14:27:07 -0300

    feat: auth function

---
+++ src/Rodonaves/auth.js
@@ -0,0 +1,11 @@
+export default async function auth() {
+  const { data } = await this.fetch('/token', 'POST', undefined, {
+    auth_type: this.mode,
+    grant_type: 'password',
+    username: this.username,
+    password: this.password,
+  });
+
+  this.token = data.access_token;
+  return true;
+}
