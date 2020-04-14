8c1711da03dc20a6296ff7e282a4f56da726dd60
Jonathan Célio <jonathan.clio@hotmail.com>
2020-04-14 11:13:56 -0300

    refactor: export axios class error

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,10 +1,12 @@
 import {
+  AxiosTestError,
   RodonavesFetchServerError,
   RodonavesFetchClientError,
   RodonavesFetchOtherError,
 } from './fetch';
 
 export {
+  AxiosTestError,
   RodonavesFetchServerError,
   RodonavesFetchClientError,
   RodonavesFetchOtherError,
