418ab993a58f7410664e912ce22145fef70b2c2b
Jonathan Célio <jonathan.clio@hotmail.com>
2020-04-14 01:23:57 -0300

    test: auth and fetch updated

--- src/Rodonaves/auth.test.js
+++ src/Rodonaves/auth.test.js
@@ -1,6 +1,6 @@
 import Rodonaves from './index';
 
-test('calls auth', async () => {
+test('calls auth and succeed', async () => {
   const rodonaves = new Rodonaves('carimflex', 'BP9AXI76');
   const auth = await rodonaves.auth();
   expect(auth).toBe(true);
--- src/Rodonaves/fetch.test.js
+++ src/Rodonaves/fetch.test.js
@@ -1,6 +1,9 @@
 import Rodonaves from './index';
+import {
+  RodonavesFetchServerError,
+} from '../errors';
 
-test('calls fetch', async () => {
+test('calls fetch and succeed', async () => {
   const rodonaves = new Rodonaves('carimflex', 'BP9AXI76');
   const response = await rodonaves.fetch('/token', 'POST', undefined, {
     auth_type: rodonaves.mode,
@@ -9,4 +12,17 @@ test('calls fetch', async () => {
     password: rodonaves.password,
   });
   expect(response).toBeTruthy();
+});
+
+test('calls fetch and throw ServerError', async () => {
+  const fetch = async () => {
+    const rodonaves = new Rodonaves('carimflex', '12345');
+    await rodonaves.fetch('/token', 'POST', undefined, {
+      auth_type: rodonaves.mode,
+      grant_type: 'password',
+      username: rodonaves.username,
+      password: rodonaves.password,
+    });
+  };
+  return expect(fetch()).rejects.toThrow(RodonavesFetchServerError);
 });
