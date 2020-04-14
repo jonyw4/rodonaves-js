2a10328fb11e3cf09365bf003477c86f319010f2
Jonathan Célio <jonathan.clio@hotmail.com>
2020-04-14 11:13:22 -0300

    test: mocking axios for make HTTP request

--- src/Rodonaves/fetch.test.js
+++ src/Rodonaves/fetch.test.js
@@ -1,28 +1,55 @@
+import axios from 'axios';
 import Rodonaves from './index';
 import {
+  AxiosTestError,
   RodonavesFetchServerError,
+  RodonavesFetchClientError,
+  RodonavesFetchOtherError,
 } from '../errors';
 
-test('calls fetch and succeed', async () => {
-  const rodonaves = new Rodonaves('carimflex', 'BP9AXI76');
-  const response = await rodonaves.fetch('/token', 'POST', undefined, {
-    auth_type: rodonaves.mode,
-    grant_type: 'password',
-    username: rodonaves.username,
-    password: rodonaves.password,
-  });
-  expect(response).toBeTruthy();
-});
+jest.mock('axios');
+axios.mockResolvedValue();
 
-test('calls fetch and throw ServerError', async () => {
-  const fetch = async () => {
-    const rodonaves = new Rodonaves('carimflex', '12345');
-    await rodonaves.fetch('/token', 'POST', undefined, {
-      auth_type: rodonaves.mode,
-      grant_type: 'password',
-      username: rodonaves.username,
-      password: rodonaves.password,
+describe('Rodonaves.fetch()', () => {
+  it('should fetch a GET request successfully from RTE API', async () => {
+    axios.mockImplementationOnce(() => Promise.resolve({
+      data: { token: 'token123' },
+    }));
+
+    const rodonaves = new Rodonaves('u', 'p');
+    const response = await rodonaves.fetch('/token', 'GET', { user: 'u', password: 'p' });
+
+    expect(response).toEqual({ token: 'token123' });
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      baseURL: 'https://01wapi.rte.com.br/',
+      url: '/token',
+      method: 'GET',
+      data: '',
+      headers: { 'Content-Type': 'multipart/form-data' },
+      params: { password: 'p', user: 'u' },
+      timeout: 1000,
     });
-  };
-  return expect(fetch()).rejects.toThrow(RodonavesFetchServerError);
+  });
+
+  it('should fetch an RodonavesOtherError from API', async () => {
+    axios.mockRejectedValue(new AxiosTestError({}));
+    const rodonaves = new Rodonaves('u', 'p');
+    const fetch = rodonaves.fetch('/test', 'GET');
+    await expect(fetch).rejects.toThrow(RodonavesFetchOtherError);
+  });
+
+  it('should fetch an RodonavesFetchClientError from API', async () => {
+    axios.mockRejectedValue(new AxiosTestError({ request: {} }));
+    const rodonaves = new Rodonaves('u', 'p');
+    const fetch = rodonaves.fetch('/test', 'GET');
+    await expect(fetch).rejects.toThrow(RodonavesFetchClientError);
+  });
+
+  it('should fetch an RodonavesFetchServerError from API', async () => {
+    axios.mockRejectedValue(new AxiosTestError({ response: { status: 404 } }));
+    const rodonaves = new Rodonaves('u', 'p');
+    const fetch = rodonaves.fetch('/test', 'GET');
+    await expect(fetch).rejects.toThrow(RodonavesFetchServerError);
+  });
 });
